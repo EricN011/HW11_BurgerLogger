57761f7103fc3ada1f01c5a7c0cb5cb005c3449a
Eric Noll <eanoll88@gmail.com>
2020-01-07 18:25:33 -0600

    cats to burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -9,7 +9,7 @@ $(function () {
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newSleepState
     }).then(
@@ -25,18 +25,18 @@ $(function () {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       name: $("#ca").val().trim(),
       sleepy: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function () {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
@@ -46,7 +46,7 @@ $(function () {
   $(".js-delete").on('click', function (event) {
     const id = $(this).data('id');
 
-    $.ajax('/api/cats/' + id, { type: 'DELETE' }).then(() => {
+    $.ajax('/api/burgers/' + id, { type: 'DELETE' }).then(() => {
       location.reload()
     })
   })
